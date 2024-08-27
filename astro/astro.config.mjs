8c5adcca74496a52fa514054f3ba4b71c771c3bf
nickespro130 <nickespro130@outlook.es>
2024-08-27 18:55:25 +0200

    DEV | v1.3.3 | Bug Fix

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,  // Configura el puerto en el que se ejecutará el servidor Astro
+    host: true,  // Permite que el servidor sea accesible externamente, no solo desde localhost
+  },
+  build: {
+    outDir: 'dist',  // Directorio de salida para la construcción de producción
+  },
+  integrations: [
+    // Puedes agregar integraciones de Astro aquí
+    // Ejemplo: '@astrojs/tailwind', '@astrojs/sitemap', etc.
+  ],
+});
